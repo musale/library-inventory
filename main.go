832e9c5f4904c7bad854ee66abbfe450e6e99e5e
Martin Musale <martinmshale@gmail.com>
2018-04-15 21:59:08 +0300

    Added sqlite3 database connection

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Page of the web application
 type Page struct {
-	Name string
+	Name     string
+	DBStatus bool
 }
 
 func main() {
+	db, _ := sql.Open("sqlite3", "dev.db")
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +24,8 @@ func main() {
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
+		p.DBStatus = db.Ping() == nil
+
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
