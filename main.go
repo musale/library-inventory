a0b4e26e35388dd5d17973759e59e0a74bd0966f
Martin Musale <martinmshale@gmail.com>
2018-04-15 22:24:16 +0300

    Added dummy data on template using jquery

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -15,18 +16,41 @@ type Page struct {
 	DBStatus bool
 }
 
+// SearchResult of a book search
+type SearchResult struct {
+	Title  string
+	Author string
+	Year   string
+	ID     string
+}
+
 func main() {
-	db, _ := sql.Open("sqlite3", "dev.db")
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
+	db, _ := sql.Open("sqlite3", "dev.db")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		p := Page{Name: "Gopher"}
+		p := Page{Name: "Gopher", DBStatus: false}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
+
 		p.DBStatus = db.Ping() == nil
 
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results := []SearchResult{
+			SearchResult{"Moby-Dick", "Herman Melville", "1851", "222222"},
+			SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "444444"},
+			SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
