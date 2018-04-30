647ab4f1dced140ce7552d55b09bb20440322e68
Martin Musale <martinmshale@gmail.com>
2018-04-30 07:22:32 +0300

    Add sessions, code to handle sort and filtering and persist values

--- main.go
+++ main.go
@@ -1,38 +1,42 @@
 package main
 
 import (
+	"net/http"
+
 	"database/sql"
+
+	"github.com/go-gorp/gorp"
+	_ "github.com/mattn/go-sqlite3"
+
 	"encoding/json"
 	"encoding/xml"
 	"io/ioutil"
-	"net/http"
 	"net/url"
+	"strconv"
 
+	"github.com/goincremental/negroni-sessions"
+	"github.com/goincremental/negroni-sessions/cookiestore"
 	gmux "github.com/gorilla/mux"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/urfave/negroni"
 	"github.com/yosssi/ace"
 )
 
-var db *sql.DB
-
-// ClassifyAPIURL URL
-const ClassifyAPIURL = "http://classify.oclc.org/classify2/Classify"
-
 // Book structure
 type Book struct {
-	PK             int
-	Title          string
-	Author         string
-	Classification string
+	PK             int64  `db:"pk"`
+	Title          string `db:"title"`
+	Author         string `db:"author"`
+	Classification string `db:"classification"`
+	ID             string `db:"id"`
 }
 
-// Page of a book
+// Page structure
 type Page struct {
-	Books []Book
+	Books  []Book
+	Filter string
 }
 
-// SearchResult of a book search
+// SearchResult structure
 type SearchResult struct {
 	Title  string `xml:"title,attr"`
 	Author string `xml:"author,attr"`
@@ -40,44 +44,85 @@ type SearchResult struct {
 	ID     string `xml:"owi,attr"`
 }
 
-// ClassifyResponse from classify2 API
-type ClassifyResponse struct {
-	Results []SearchResult `xml:"works>work"`
+var db *sql.DB
+var dbmap *gorp.DbMap
+
+const ClassifyAPIURL = "http://classify.oclc.org/classify2/Classify"
+
+func initDb() {
+	db, _ = sql.Open("sqlite3", "dev.db")
+
+	dbmap = &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+
+	dbmap.AddTableWithName(Book{}, "books").SetKeys(true, "pk")
+	dbmap.CreateTablesIfNotExists()
 }
 
-// BookResponse from classify2 API
-type BookResponse struct {
-	BookData struct {
-		Title  string `xml:"title,attr"`
-		Author string `xml:"author,attr"`
-		ID     string `xml:"owi,attr"`
-	} `xml:"work"`
-	Classification struct {
-		MostPopular string `xml:"sfa,attr"`
-	} `xml:"recommendations>ddc>mostPopular"`
+func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if err := db.Ping(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	next(w, r)
 }
 
-// ClassifyBookResponse structure
-type ClassifyBookResponse struct {
-	BookData struct {
-		Title  string `xml:"title,attr"`
-		Author string `xml:"author,attr"`
-		ID     string `xml:"owi,attr"`
-	} `xml:"work"`
-	Classification struct {
-		MostPopular string `xml:"sfa,attr"`
-	} `xml:"recommendations>ddc>mostPopular"`
+func getBookCollection(books *[]Book, sortCol string, filterByClass string, w http.ResponseWriter) bool {
+	if sortCol == "" {
+		sortCol = "pk"
+	}
+	var where string
+	if filterByClass == "fiction" {
+		where = " where classification between '800' and '900'"
+	} else if filterByClass == "nonfiction" {
+		where = " where classification not between '800' and '900'"
+	}
+	if _, err := dbmap.Select(books, "select * from books"+where+" order by "+sortCol); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return false
+	}
+	return true
 }
 
-// SearchResponse structure
-type SearchResponse struct {
-	Results []SearchResult `xml:"works>work"`
+func getStringFromSession(r *http.Request, key string) string {
+	var strVal string
+	if val := sessions.GetSession(r).Get(key); val != nil {
+		strVal = val.(string)
+	}
+	return strVal
 }
 
 func main() {
-	db, _ = sql.Open("sqlite3", "dev.db")
+	initDb()
 
 	mux := gmux.NewRouter()
+
+	mux.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		var b []Book
+		if !getBookCollection(&b, getStringFromSession(r, "SortBy"), r.FormValue("filter"), w) {
+			return
+		}
+
+		sessions.GetSession(r).Set("Filter", r.FormValue("filter"))
+
+		if err := json.NewEncoder(w).Encode(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}).Methods("GET").Queries("filter", "{filter:all|fiction|nonfiction}")
+
+	mux.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		var b []Book
+		if !getBookCollection(&b, r.FormValue("sortBy"), getStringFromSession(r, "Filter"), w) {
+			return
+		}
+
+		sessions.GetSession(r).Set("SortBy", r.FormValue("sortBy"))
+
+		if err := json.NewEncoder(w).Encode(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}).Methods("GET").Queries("sortBy", "{sortBy:title|author|classification}")
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		template, err := ace.Load("templates/index", "", nil)
@@ -86,12 +131,9 @@ func main() {
 			return
 		}
 
-		p := Page{Books: []Book{}}
-		rows, _ := db.Query("select pk,title,author,classification from books")
-		for rows.Next() {
-			var b Book
-			rows.Scan(&b.PK, &b.Title, &b.Author, &b.Classification)
-			p.Books = append(p.Books, b)
+		p := Page{Books: []Book{}, Filter: getStringFromSession(r, "Filter")}
+		if !getBookCollection(&p.Books, getStringFromSession(r, "SortBy"), getStringFromSession(r, "Filter"), w) {
+			return
 		}
 
 		if err = template.Execute(w, p); err != nil {
@@ -105,6 +147,7 @@ func main() {
 
 		if results, err = search(r.FormValue("search")); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
 		encoder := json.NewEncoder(w)
@@ -122,28 +165,25 @@ func main() {
 			return
 		}
 
-		result, err := db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
-			nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		pk, _ := result.LastInsertId()
 		b := Book{
-			PK:             int(pk),
+			PK:             -1,
 			Title:          book.BookData.Title,
 			Author:         book.BookData.Author,
 			Classification: book.Classification.MostPopular,
 		}
+		if err = dbmap.Insert(&b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 		if err := json.NewEncoder(w).Encode(b); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}).Methods("PUT")
 
 	mux.HandleFunc("/books/{pk}", func(w http.ResponseWriter, r *http.Request) {
-		if _, err := db.Exec("delete from books where pk = ?", gmux.Vars(r)["pk"]); err != nil {
+		pk, _ := strconv.ParseInt(gmux.Vars(r)["pk"], 10, 64)
+		if _, err := dbmap.Delete(&Book{pk, "", "", "", ""}); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -151,38 +191,27 @@ func main() {
 	}).Methods("DELETE")
 
 	n := negroni.Classic()
+	n.Use(sessions.Sessions("go-for-web-dev", cookiestore.New([]byte("my-secret-123"))))
 	n.Use(negroni.HandlerFunc(verifyDatabase))
 	n.UseHandler(mux)
-
 	n.Run(":8080")
 }
 
-func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	if err := db.Ping(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	next(w, r)
+// ClassifySearchResponse structure
+type ClassifySearchResponse struct {
+	Results []SearchResult `xml:"works>work"`
 }
 
-func fetch(id string) (BookResponse, error) {
-	var b BookResponse
-	body, err := classifyAPI(ClassifyAPIURL + "?owi=" + url.QueryEscape(id) + "&summary=true")
-	if err != nil {
-		return b, err
-	}
-	err = xml.Unmarshal(body, &b)
-	return b, err
-}
-
-func search(query string) ([]SearchResult, error) {
-	var s SearchResponse
-	body, err := classifyAPI(ClassifyAPIURL + "?title=" + url.QueryEscape(query) + "&summary=true")
-	if err != nil {
-		return s.Results, nil
-	}
-	err = xml.Unmarshal(body, &s)
-	return s.Results, nil
+// ClassifyBookResponse structure
+type ClassifyBookResponse struct {
+	BookData struct {
+		Title  string `xml:"title,attr"`
+		Author string `xml:"author,attr"`
+		ID     string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recommendations>ddc>mostPopular"`
 }
 
 func find(id string) (ClassifyBookResponse, error) {
@@ -195,6 +224,18 @@ func find(id string) (ClassifyBookResponse, error) {
 
 	err = xml.Unmarshal(body, &c)
 	return c, err
+}
+
+func search(query string) ([]SearchResult, error) {
+	var c ClassifySearchResponse
+	body, err := classifyAPI(ClassifyAPIURL + "?summary=true&title=" + url.QueryEscape(query))
+
+	if err != nil {
+		return []SearchResult{}, err
+	}
+
+	err = xml.Unmarshal(body, &c)
+	return c.Results, err
 }
 
 func classifyAPI(url string) ([]byte, error) {
